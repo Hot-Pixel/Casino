22c59608b2c159ad8735b89eedc9b9802d250363
entumas <entumas@gmail.com>
2022-07-06 21:09:58 +0200

    Module Carousel-bets v2

--- public/js/main.js
+++ public/js/main.js
@@ -32,11 +32,17 @@
     var carousel = new Splide('.js--splide--carousel-bets', {
       type: 'loop',
       pagination: false,
-      perPage: 4,
+      perPage: 5,
       perMove: 1,
       gap: 15,
       breakpoints: {
+        1600: {
+          perPage: 4
+        },
         1200: {
+          perPage: 3
+        },
+        992: {
           perPage: 2
         }
       }
--- src/js/carouselBets.js
+++ src/js/carouselBets.js
@@ -2,11 +2,17 @@ function carouselBets() {
 	var carousel = new Splide( '.js--splide--carousel-bets', {
 		type       : 'loop',
 		pagination : false,
-		perPage    : 4,
+		perPage    : 5,
 		perMove    : 1,
 		gap        : 15,
 		breakpoints: {
+			1600: {
+				perPage : 4,
+			},
 			1200: {
+				perPage : 3,
+			},
+			992: {
 				perPage : 2,
 			},
 		}
