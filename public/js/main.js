ba47ddcafc5fb33eb340eb45049f1cde683d5ccb
Santiago <info@santiagomartinez.co>
2022-07-11 16:35:09 +0200

    Grid Casino Comenzado

--- public/js/main.js
+++ public/js/main.js
@@ -89,8 +89,6 @@
   const carouselJackpotExist = document.getElementsByClassName("js--splide--carousel-jackpot");
   const carouselBetsExist = document.getElementsByClassName("js--splide--carousel-bets");
   const gridHalfExist = document.getElementsByClassName("m-grid__half");
-  const arrowArr$1 = document.querySelectorAll(".js-arrow__grid");
-  const gridArr$1 = document.querySelectorAll(".o-grid__half--grid");
   window.addEventListener("load", () => {
     if (carouselBannerExist.length > 0) {
       carouselBanner();
@@ -107,6 +105,29 @@
     if (gridHalfExist.length > 0) {
       collapseGrid();
     }
+  });
+  var options = {
+    valueNames: ["name", {
+      data: ["type"]
+    }]
+  };
+  var casinoGridFilter = new List("casino-filter", options);
+  console.log(casinoGridFilter);
+  const btnAll = document.querySelector("#casino-all");
+  btnAll.addEventListener("click", () => {
+    casinoGridFilter.filter();
+  });
+  const btnJack = document.querySelector("#casino-blackjack");
+  btnJack.addEventListener("click", () => {
+    casinoGridFilter.filter(function (item) {
+      return item.values().type == "blackjack";
+    });
+  });
+  const btnRuleta = document.querySelector("#casino-ruleta");
+  btnRuleta.addEventListener("click", () => {
+    casinoGridFilter.filter(function (item) {
+      return item.values().type == "ruleta";
+    });
   });
 
 })));
--- src/js/main.js
+++ src/js/main.js
@@ -4,25 +4,52 @@ import carouselBets from "./carouselBets.js";
 import collapseGrid from "./collapseGridHalf.js";
 
 const carouselBannerExist = document.getElementsByClassName("m-banner__car");
-const carouselJackpotExist = document.getElementsByClassName("js--splide--carousel-jackpot");
-const carouselBetsExist = document.getElementsByClassName("js--splide--carousel-bets");
+const carouselJackpotExist = document.getElementsByClassName(
+  "js--splide--carousel-jackpot"
+);
+const carouselBetsExist = document.getElementsByClassName(
+  "js--splide--carousel-bets"
+);
 const gridHalfExist = document.getElementsByClassName("m-grid__half");
 
-const arrowArr = document.querySelectorAll(".js-arrow__grid");
-const gridArr = document.querySelectorAll(".o-grid__half--grid");
-
 window.addEventListener("load", () => {
-	if (carouselBannerExist.length > 0) {
-		carouselBanner();
-	}
-	if (carouselJackpotExist.length > 0) {
-		carouselJackpot();
-	}
-	if (carouselBetsExist.length > 0) {
-		carouselBets();
-	}
-	if (gridHalfExist.length > 0) {
-		collapseGrid();
-	}
+  if (carouselBannerExist.length > 0) {
+    carouselBanner();
+  }
+  if (carouselJackpotExist.length > 0) {
+    carouselJackpot();
+  }
+  if (carouselBetsExist.length > 0) {
+    carouselBets();
+  }
+  if (gridHalfExist.length > 0) {
+    collapseGrid();
+  }
 });
 
+var options = {
+  valueNames: ["name", {data: ["type"]}],
+};
+
+var casinoGridFilter = new List("casino-filter", options);
+
+console.log(casinoGridFilter);
+
+const btnAll = document.querySelector("#casino-all");
+btnAll.addEventListener("click", () => {
+  casinoGridFilter.filter();
+});
+
+const btnJack = document.querySelector("#casino-blackjack");
+btnJack.addEventListener("click", () => {
+  casinoGridFilter.filter(function (item) {
+    return item.values().type == "blackjack";
+  });
+});
+
+const btnRuleta = document.querySelector("#casino-ruleta");
+btnRuleta.addEventListener("click", () => {
+  casinoGridFilter.filter(function (item) {
+    return item.values().type == "ruleta";
+  });
+});
