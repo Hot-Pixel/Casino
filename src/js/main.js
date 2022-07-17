3245f7e74f63c3fc3ecd6bb2646d2b736832af8e
Santiago <info@santiagomartinez.co>
2022-07-17 12:04:07 +0200

    Filtro Casino Actualizado

--- src/js/main.js
+++ src/js/main.js
@@ -27,71 +27,30 @@ window.addEventListener("load", () => {
   }
 });
 
-var options = {
-  valueNames: [
-    { data: ["type"] },
-    { data: ["provider"] },
-    { data: ["fav"] }
-  ],
-};
-
-var casinoGridFilter = new List("casino-filter", options);
-
-const btnAll = document.querySelector("#casino-all");
-btnAll.addEventListener("click", () => {
-  casinoGridFilter.filter();
-});
-
-const btnJack = document.querySelector("#casino-blackjack");
-btnJack.addEventListener("click", () => {
-  casinoGridFilter.filter(function (item) {
-    return item.values().type == "blackjack";
-  });
-});
-
-const btnRuleta = document.querySelector("#casino-ruleta");
-btnRuleta.addEventListener("click", () => {
-  casinoGridFilter.filter(function (item) {
-    return item.values().type == "ruleta";
-  });
-});
-
-const selectCasino = document.querySelector("#casino-provider");
-
-selectCasino.addEventListener("change", () => {
-  if (selectCasino.value == "one") {
-    casinoGridFilter.filter(function (item) {
-      return item.values().provider == "one";
-    });
-  } else if (selectCasino.value == "two") {
-    casinoGridFilter.filter(function (item) {
-      return item.values().provider == "two";
-    });
-  } else if (selectCasino.value == "three") {
-    casinoGridFilter.filter(function (item) {
-      return item.values().provider == "three";
-    });
-  }
-});
-
 const hearts = document.querySelectorAll(".o-casino__grid-fav");
 
 hearts.forEach((heart) => {
   heart.addEventListener("click", () => {
     heart.classList.toggle("fav");
     if (heart.classList.contains("fav")) {
-      heart.parentElement.parentElement.setAttribute("data-fav", "true");
+      heart.parentElement.parentElement.classList.add("favourite");
     } else {
-      heart.parentElement.parentElement.setAttribute("data-fav", "false");
+      heart.parentElement.parentElement.classList.remove("favourite");
     }
   });
 });
 
-const btnFav = document.querySelector("#casino-fav");
-btnFav.addEventListener("click", () => {
-  casinoGridFilter.reIndex();
-  casinoGridFilter.filter(function (item) {
-    console.log(item.values())
-    return item.values().fav === "true";
-  });
+var mixer = mixitup(".m-casino__filter", {
+  multifilter: {
+    enable: true,
+  },
+  selectors: {
+    target: ".o-casino__grid-item",
+  },
+  controls: {
+    enable: true,
+  },
+  animation: {
+    enable: false,
+  },
 });
