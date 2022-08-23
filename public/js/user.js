324a8e05014c0b5b085c2c370ecade862686ef2a
Santiago <info@santiagomartinez.co>
2022-08-23 21:02:04 +0200

    Mis Datos Modificar y Mi Cartera Terminados

--- public/js/user.js
+++ public/js/user.js
@@ -1,23 +1,21 @@
-import { g as gsapWithCSS } from './index-92880765.js';
+document.querySelector(".user");
+document.querySelector("header.header");
+document.querySelector(".userScreen");
+document.querySelector(".userMenu");
 
-const userBtn = document.querySelector(".user");
-const header = document.querySelector("header.header");
-const userScreen = document.querySelector(".userScreen");
-const userMenu = document.querySelector(".userMenu");
-
-userBtn.addEventListener("click", () => {
-  header.classList.toggle("header--light");
-  userScreen.classList.toggle("user-on");
-  if (userScreen.classList.contains("user-on")) {
-    gsapWithCSS
-      .timeline()
-      .to(userScreen, { opacity: 1, duration: 0.4 })
-      .to(userMenu, { left: 0, opacity: 1, duration: 0.4 });
-  } else {
-    gsapWithCSS
-      .timeline()
-      .to(userMenu, { left: "-15%", opacity: 0, duration: 0.4 })
-      .to(userScreen, { opacity: 0, duration: 0.4 });
-  }
-});
+// userBtn.addEventListener("click", () => {
+//   header.classList.toggle("header--light");
+//   userScreen.classList.toggle("user-on");
+//   if (userScreen.classList.contains("user-on")) {
+//     gsap
+//       .timeline()
+//       .to(userScreen, { opacity: 1, duration: 0.4 })
+//       .to(userMenu, { left: 0, opacity: 1, duration: 0.4 });
+//   } else {
+//     gsap
+//       .timeline()
+//       .to(userMenu, { left: "-15%", opacity: 0, duration: 0.4 })
+//       .to(userScreen, { opacity: 0, duration: 0.4 });
+//   }
+// });
 //# sourceMappingURL=user.js.map
--- src/js/user.js
+++ src/js/user.js
@@ -5,18 +5,18 @@ const header = document.querySelector("header.header");
 const userScreen = document.querySelector(".userScreen");
 const userMenu = document.querySelector(".userMenu");
 
-userBtn.addEventListener("click", () => {
-  header.classList.toggle("header--light");
-  userScreen.classList.toggle("user-on");
-  if (userScreen.classList.contains("user-on")) {
-    gsap
-      .timeline()
-      .to(userScreen, { opacity: 1, duration: 0.4 })
-      .to(userMenu, { left: 0, opacity: 1, duration: 0.4 });
-  } else {
-    gsap
-      .timeline()
-      .to(userMenu, { left: "-15%", opacity: 0, duration: 0.4 })
-      .to(userScreen, { opacity: 0, duration: 0.4 });
-  }
-});
+// userBtn.addEventListener("click", () => {
+//   header.classList.toggle("header--light");
+//   userScreen.classList.toggle("user-on");
+//   if (userScreen.classList.contains("user-on")) {
+//     gsap
+//       .timeline()
+//       .to(userScreen, { opacity: 1, duration: 0.4 })
+//       .to(userMenu, { left: 0, opacity: 1, duration: 0.4 });
+//   } else {
+//     gsap
+//       .timeline()
+//       .to(userMenu, { left: "-15%", opacity: 0, duration: 0.4 })
+//       .to(userScreen, { opacity: 0, duration: 0.4 });
+//   }
+// });
