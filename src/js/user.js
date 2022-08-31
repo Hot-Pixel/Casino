ab5c092650a9f4c60a91f07275734ea747d368b6
Santiago <info@santiagomartinez.co>
2022-08-31 12:19:13 +0200

    Actualizacion

--- src/js/user.js
+++ src/js/user.js
@@ -102,3 +102,47 @@ menuMobileCollapse.addEventListener("click", () => {
 menuMobileTab.addEventListener("click", () => {
   gsap.to(userMenuMobile, { right: 0, opacity: 1, duration: 0.4 });
 });
+
+const verifyBtn = document.querySelectorAll(".nav__btn > button");
+const docsBlock = document.querySelector(".docsBlock");
+const docsVerifyBlock = document.querySelector(".docsVerifyBlock");
+const docsVerifyBlock2 = document.querySelector(".docsVerifyBlock2");
+const docsArr = [docsBlock, docsVerifyBlock, docsVerifyBlock2];
+
+verifyBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    blockArr.forEach((block) => {
+      block.classList.remove("block-active");
+    });
+    docsArr.forEach((doc) => {
+      doc.classList.remove("block-active");
+    });
+    blockArr[1].classList.add("block-active");
+    docsVerifyBlock.classList.add("block-active");
+  });
+});
+
+const withdrawOpenBtn = document.querySelector(".wallet__btn-withdraw");
+const whitdrawBlock = document.querySelector(".whitdrawScreen");
+const withdrawCloseBtn = document.querySelector(".withdraw__btn-close");
+
+withdrawOpenBtn.addEventListener("click", () => {
+  whitdrawBlock.classList.add("block-active");
+});
+
+withdrawCloseBtn.addEventListener("click", () => {
+  whitdrawBlock.classList.remove("block-active");
+});
+
+const btnAmmountArr = document.querySelectorAll(".btnAmmount");
+const inputAmmount = document.querySelector(".depositDetail__ammount");
+
+btnAmmountArr[0].addEventListener('click', () => {
+  inputAmmount.value = 20;
+})
+btnAmmountArr[1].addEventListener('click', () => {
+  inputAmmount.value = 50;
+})
+btnAmmountArr[2].addEventListener('click', () => {
+  inputAmmount.value = 100;
+})
