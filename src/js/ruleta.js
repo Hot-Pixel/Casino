7c8240ec55d912178ee5c1d685d9f3cc1a7a05e9
Josep Camps <jcamps@atoomstudio.com>
2022-09-21 18:39:15 +0200

    Remove dataModify from ruleta.js

--- src/js/ruleta.js
+++ src/js/ruleta.js
@@ -1,6 +1,6 @@
 import popUpSaldo from "./modules/popUpBalance.js";
 import marginHeader from "./modules/marginHeader.js";
-import dataModify from "./modules/dataModify.js";
+// import dataModify from "./modules/dataModify.js";
 import favHeart from "./modules/favHeart.js";
 import filterWheel from "./modules/filterWheel.js";
 import depositSteps from "./modules/depositSteps.js";
@@ -9,7 +9,7 @@ import depositAmmount from "./modules/depositAmmount.js";
 window.addEventListener('load', () => {
   popUpSaldo()
   marginHeader()
-  dataModify()
+  // dataModify()
   favHeart()
   depositSteps()
   depositAmmount()
