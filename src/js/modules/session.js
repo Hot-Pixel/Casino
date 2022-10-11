787147a76bc294d952f323918a36288847e3b093
Josep Camps <jcamps@atoomstudio.com>
2022-10-11 13:01:46 +0200

    Remove verificaInactividad from startTimer1

--- src/js/modules/session.js
+++ src/js/modules/session.js
@@ -7,11 +7,6 @@ function Session() {
     function startTimeWeb1(delay) {
         init = new Date().getTime() - delay;
         let t = setTimeout(function () { startTimeWeb() }, 1000);
-
-        window.addEventListener('focus', function () {
-            console.log('Focus');
-            verificaInactividad();
-        });
     }
 
     function startTimeWeb() {
@@ -49,15 +44,10 @@ function Session() {
         fetch('/session.jsp')
             .then(res => res.json())
             .then(data => {
-                if (data.status == 'ok') {
-                    console.log('Result ' + data.result);
-                    quering = false;
-                    if (!data.result) {
-                        mpu('/inactividadWeb.htm');
-                    }
-                } else {
-                    console.log('Error ' + data.error);
-                    quering = false;
+                console.log('Result ' + data.result);
+                quering = false;
+                if (!data.result) {
+                    mpu('/inactividadWeb.htm');
                 }
             })
             .catch(err => {
@@ -69,6 +59,62 @@ function Session() {
     function lastSessionInfo() {
         window.open('/logout.jsp', '_self');
     }
+
+    // TODO: Make lastSessionInfo popup
+    /*
+    function lastSessionInfo() {
+
+        if (!String.prototype.startsWith) {
+            String.prototype.startsWith = function (search, pos) {
+                return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+            };
+        }
+        url = "/servlet/sessionSummary?rnd=" + new Date();
+        $.ajax({
+            dataType: "json",
+            data: "",
+            url: url,
+            success: function (json) {
+                console.log("|lastSessionInfo|");
+                console.log(json);
+                if (json.result == "ok" && json.data.length > 0) {
+    
+                    if (window.location.href.startsWith("https://www.casinobarcelona.es/sportncoheader-apuestas2.html")) {
+                        console.log("Sending OPENING message");
+                        parent.postMessage({ "method": "OPENING" }, "https://apuestas.casinobarcelona.es/");
+                    }
+    
+                    var table = "<div id='tablecontainer'><table class='tables' id='resumenSession'><thead>";
+                    table += "<tr><th>Juego</th><th>Apostado</th><th>Ganado</th></tr></thead><tbody>";
+                    for (var i = 0; i < json.data.length; i++) {
+                        var wagers = json.data[i].wagers;
+                        var winnings = json.data[i].winnings;
+    
+                        table += "<tr><td title='Juego'>" + json.data[i].room_name + "</td><td title='Apostado'>" + wagers.formatMoney(2) + "</td><td title='Ganado'>" + winnings.formatMoney(2) + "</td></tr>";
+                    }
+                    table += "</tbody></table></div>";
+                    $('#summaryData').html(table);
+    
+                    $("#lastSlotSessionInfo").show();
+                    window.scrollTo(0, 0);
+                    eraseCookie('logueado');
+    
+                } else {
+    
+                    eraseCookie('logueado');
+    
+                    if (window.location.href.startsWith("https://www.casinobarcelona.es/sportncoheader-apuestas2.html")) {
+                        console.log("Sending LOGOUT message");
+                        parent.postMessage({ "method": "LOGOUT" }, "https://apuestas.casinobarcelona.es/");
+                        window.open('/logout.jsp', '_parent');
+                    } else {
+                        window.open('/logout.jsp', '_self');
+                    }
+                }
+            }
+        });
+    }
+    */
 
     return {
         startTimeWeb1,
