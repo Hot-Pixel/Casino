97d8d4f48eeeb0eefc6a9af30ac7581773e14794
Josep Camps <jcamps@atoomstudio.com>
2022-10-21 23:56:48 +0200

    Manage support form

--- src/js/support.js
+++ src/js/support.js
@@ -1,8 +1,33 @@
 import historyNavBar from "./modules/historyNavBar.js";
 import loading from "./modules/loading.js";
 
-loading()
+loading();
+
+function initSupportForm() {
+  const theForm = document.querySelector("#supportForm")
+  const alertMgr = Alert();
+
+  const sendMail = async () => {
+    const response = await fetch("https://api.casinobarcelona.es/mail/support/send", {
+      method: "post",
+      body: new FormData(theForm),
+    })
+    if (response.ok) {
+      alertMgr.add("Consulta enviada", "success")
+    } else {
+      alertMgr.add("Algo ha salido mal, prueba mas tarde o abre el chat", "error")
+    }
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    sendMail();
+  }
+
+  theForm.onsubmit = submitForm;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   historyNavBar();
+  initSupportForm();
 })
