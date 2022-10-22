52346f9e71e0b5749098e970bbe8f786c1de9f97
Josep Camps <jcamps@atoomstudio.com>
2022-10-22 18:27:29 +0200

    Disable carousel-bets

--- src/js/home.js
+++ src/js/home.js
@@ -1,13 +1,13 @@
 import carouselBanner from "./modules/carouselBanner.js";
 import carouselJackpot from "./modules/carouselJackpot.js";
-import carouselBets from "./modules/carouselBets.js";
+// import carouselBets from "./modules/carouselBets.js";
 import collapseGrid from "./modules/collapseGridHalf.js";
 import carouselGrid from "./modules/carouselGrid.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   carouselBanner();
   carouselJackpot();
-  carouselBets();
+  // carouselBets();
   collapseGrid();
 
   document.querySelectorAll('.gridFull .splide').forEach(grid => {
