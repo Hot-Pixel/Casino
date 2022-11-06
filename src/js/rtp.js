1b74901cd3ded0b8437988a4cc73e8a742f2fc84
Josep Camps <jcamps@atoomstudio.com>
2022-11-06 21:51:45 +0100

    Fix accordion on rtp

--- src/js/rtp.js
+++ src/js/rtp.js
@@ -1,7 +1,5 @@
-import accordion from './modules/accordion';
 import fillRtpReal from './modules/rtp-real';
 import fillRtpTeorico from './modules/rtp-teorico';
 
-accordion();
-// fillRtpTeorico();
+fillRtpTeorico();
 fillRtpReal();
