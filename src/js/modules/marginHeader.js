09b9728aa379893c447d9bfdb5b73fb95fab0ea2
Josep Camps <jcamps@atoomstudio.com>
2022-11-10 23:35:28 +0100

    Enable marginHeader for userarea

--- src/js/modules/marginHeader.js
+++ src/js/modules/marginHeader.js
@@ -1,8 +1,7 @@
 function marginHeader() {
-  return; 
   const header = document.querySelector(".header");
-  const headerMobile = document.querySelector(".header__mobile--top");
-  const contentBlocks = document.querySelectorAll(".has-header");
+  // const headerMobile = document.querySelector(".header__mobile--top");
+  // const contentBlocks = document.querySelectorAll(".has-header");
   const userMenus = document.querySelectorAll(".userMenu");
 
   marginAction();
@@ -10,16 +9,16 @@ function marginHeader() {
 
   function marginAction() {
     if (window.innerWidth < 1280) {
-      contentBlocks.forEach((block) => {
-        block.style.paddingTop = `${headerMobile.offsetHeight}px`;
-      });
+      // contentBlocks.forEach((block) => {
+      //   block.style.paddingTop = `${headerMobile.offsetHeight}px`;
+      // });
     } else {
-      contentBlocks.forEach((block) => {
-        block.style.paddingTop = `${header.offsetHeight}px`;
-      });
+      // contentBlocks.forEach((block) => {
+      //   block.style.paddingTop = `${header.offsetHeight}px`;
+      // });
 
       userMenus.forEach((menu) => {
-        menu.style.top = `${header.offsetHeight}px`;
+        // menu.style.top = `${header.offsetHeight}px`;
         menu.style.height = `calc(100vh - ${header.offsetHeight}px)`;
       });
     }
