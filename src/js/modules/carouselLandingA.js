6c0be2556e4754d1c125ab944b267826009b5e4d
Josep Camps <jcamps@atoomstudio.com>
2022-11-27 23:15:42 +0000

    Landing sizes

--- src/js/modules/carouselLandingA.js
+++ src/js/modules/carouselLandingA.js
@@ -5,17 +5,20 @@ function carouselLandingA() {
     var carousel = new Splide(".logosLandingA__slider.splide", {
         pagination: false,
         arrows: true,
-        perPage: 4,
+        perPage: 5,
         padding: 50,
         perMove: 1,
         gap: 50,
         breakpoints: {
-        768: {
-            perPage: 3,
-        },
-        576: {
-            perPage: 2,
-        }
+            768: {
+                perPage: 4,
+            },
+            576: {
+                perPage: 3,
+            },
+            376: {
+                perPage: 2,
+            }
         }
     });
 
