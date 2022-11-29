7a296947eea78cfae548e3b06d774db23e956f3f
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-11-29 15:12:02 +0100

    Changed casino filter buttons for links

--- src/js/casino.js
+++ src/js/casino.js
@@ -6,3 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
   favHeart().init();
 })
 
+ const gameSelect = document.querySelector("#casino-game");
+    const selectLinker = ()=>{
+        if (gameSelect.value == "all"){
+            window.location.href = "/casino.html";
+        }else if (gameSelect.value == ".RULETAS"){
+            window.location.href = "/casino/ruleta.html";
+        }else {
+            window.location.href = "/casino/blackjack-online.html";
+        }
+    }
+    gameSelect.addEventListener("change", selectLinker)
